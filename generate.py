45a13e1eff6fb133dbfe15c2fd44d4e04d7b4f31
Jasper Hugunin <jasper@hugunin.net>
2026-01-11 08:49:07 -0800

    Notes, and the first third of the splitting algorithm

--- generate.py
+++ generate.py
@@ -8,8 +8,10 @@ import colorama
 
 import NetUtils
 import Utils
-from Utils import restricted_loads
+from Utils import output_path, restricted_dumps, restricted_loads
 from NetUtils import MultiData
+
+ARCHIPELAGO_DATA_VERSION = 3
 
 def load(multidata_filename: str) -> MultiData:
     # Based off of MultiServer.Context.load
@@ -27,12 +29,71 @@ def load(multidata_filename: str) -> MultiData:
             data_bytes = f.read()
 
     format_version = data_bytes[0]
-    if format_version != 3:
+    if format_version != ARCHIPELAGO_DATA_VERSION:
         raise Utils.VersionException("Incompatible multidata.")
 
     data = restricted_loads(zlib.decompress(data_bytes[1:]))
     # logging.debug(pprint.pformat(data, width=160))
     return data
+
+def write_shard(data: MultiData, shard_index: int):
+    seed = data["seed_name"]
+    data_bytes = zlib.compress(restricted_dumps(data), 9)
+    filename = f"AP_{seed}_Shard{shard_index:02}.archipelago"
+    with open(output_path(filename), "wb") as f:
+        f.write(bytes([ARCHIPELAGO_DATA_VERSION]))
+        f.write(data_bytes)
+    logging.info(f"Finished writing {filename}.")
+
+PROXY_SLOT = 1
+
+def split_multidata(data: MultiData, num_shards: int) -> list[MultiData]:
+    # Shards will be numbered 0 - (num_shards - 1)
+    slot_ids = sorted(data["slot_info"].keys())
+    assert slot_ids
+    assert slot_ids == list(range(1, slot_ids[-1] + 1))
+    assert len(slot_ids) == slot_ids[-1]
+    num_slots = len(slot_ids)
+    logging.debug(f"Slots are correctly numbered 1 - {num_slots}")
+
+    # We will give the proxy slot 1 in each shard.
+    # Distribute slots round-robin among shards
+    # Round-robin is nice because it is deterministic, simple, and should
+    # break up clumps.
+    def map_slot(slot: int) -> (int, int):
+        """Returns (shard, slot)"""
+        base_slot, shard = divmod(slot - 1, num_shards)
+        return (shard, base_slot + 2)
+
+    # Our item and location ids will be chosen cleverly.
+    # Must be integers > 0 for Archipelago protocol.
+    # For the i-th (0-indexed) item in shard r which is found in a location in a different shard,
+    # we create a location (i * num_shards + r + 1) in the proxy for shard r and an item with the same id
+    # in the other shard.
+    shard_counters = [0 for i in range(num_shards)]
+    # map from (slot, id) to (slot, proxy id). If the item/location and it's pair are both in the
+    # same shard, this is the identity map.
+    # translate_items[(x, y)] gives the proxy item in the shard where item (x, y) is found.
+    translate_items: dict[(int, int), (int, int)] = {}
+    # translate_locations[(x, y)] gives the proxy location in the shard that location (x, y) sends to.
+    translate_locations: dict[(int, int), (int, int)] = {}
+
+    # TODO: I can't follow the translation logic on this little sleep.
+    # Figure out what works consistently. Maybe work backwards, try the generation and see what you need.
+    for location_slot, slot_locations in data["locations"].items():
+        for location_id, (item_id, item_slot, _item_flags) in slot_locations.items():
+            assert (location_slot, location_id) not in translate_locations
+            assert (item_slot, item_id) not in translate_items
+            location_shard, location_new_slot = map_slot(location_slot)
+            item_shard, item_new_slot = map_slot(item_slot)
+            if location_shard == item_shard:
+                translate_items[(item_slot, item_id)] = (item_slot, item_id)
+                translate_locations[(location_slot, location_id)] = (location_slot, location_id)
+            else:
+                new_id = shard_counters[item_shard] * num_shards + item_shard + 1
+                shard_counters[item_shard] += 1
+                translate_items[(item_slot, item_id)] = (PROXY_SLOT, new_id)
+                translate_locations[(location_slot, location_id)] = (PROXY_SLOT, new_id)
 
 def get_multidata_filename(data_filename: str | None=None):
     # Mimic MultiServer for opening the .archipelago file
